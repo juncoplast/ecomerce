5dacb58a0c727ece4dbce1fdb6b07ffdcb3c3b66
juncoplast <juncoplastinfra@gmail.com>
2025-05-06 14:06:02 -0300

    fix

--- components/ColorSelector.tsx
+++ components/ColorSelector.tsx
@@ -7,6 +7,8 @@ import {
   SelectItem,
   SelectValue,
 } from "@/components/ui/select";
+import Link from "next/link";
+import { HomeIcon } from 'lucide-react'
 
 type Props = {
   colors: string[];
@@ -17,7 +19,12 @@ type Props = {
 export function ColorSelector({ colors, selectedColor, onSelect }: Props) {
   return (
     <section className="flex flex-col">
-      <h2 className="text-xl font-semibold text-primary mb-2">Escolha a Cor</h2>
+      <div className="flex flex-row text-primary ">
+        <Link href="/">
+          <HomeIcon className="mr-4"></HomeIcon>
+        </Link>
+        <h2 className="text-xl font-semibold text-primary mb-2">Escolha a Cor</h2>
+      </div>
       <Select value={selectedColor} onValueChange={onSelect}>
         <SelectTrigger className="w-full text-lg font-semibold border border-primary rounded-lg shadow-sm">
           <SelectValue />
